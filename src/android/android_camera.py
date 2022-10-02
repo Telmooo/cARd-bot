1ced83532a9e401e9f9f42172da035b7bca40d67
Ivo Saavedra <ivo_saavedra@outlook.com>
2022-10-02 17:28:20 +0100

    Add: aruco marker Add: marker detection

--- src/android/android_camera.py
+++ src/android/android_camera.py
@@ -1,8 +1,10 @@
+from turtle import position
 from typing import Literal, Optional, Tuple
 
 import numpy as np
 import cv2
 import requests
+
 
 class AndroidCamera:
     mode: str
@@ -59,3 +61,45 @@ class AndroidCamera:
             frame = cv2.resize(frame, self.img_size)
         
         return frame
+
+
+def detect_marker(frame, arucoDict, arucoParams):
+    (corners, _ids, _rejected) = cv2.aruco.detectMarkers(frame, arucoDict, parameters=arucoParams)
+
+    if len(corners) > 0:    
+        for (x,y) in corners[0][0]:
+            cv2.drawMarker(frame, (int(x), int(y)), color=(0,255,0), markerType=cv2.MARKER_TILTED_CROSS, thickness=2)
+
+def print_aruco_params(paramDict):
+    '''
+    https://docs.opencv.org/3.4/d1/dcd/structcv_1_1aruco_1_1DetectorParameters.html#acf6a0da0ae6bcd582c51ae14263df87a
+    '''
+    for param in dir(paramDict):
+        if not param.startswith('__'):
+            print(f'{param} = {getattr(paramDict, param)}')
+
+
+if __name__ == "__main__":
+
+    camera = AndroidCamera("usb", device_no=1, img_size=(1280,720))
+
+    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_ARUCO_ORIGINAL)
+    arucoParams = cv2.aruco.DetectorParameters_create()
+
+    print_aruco_params(arucoParams)
+    while True:
+        try:
+            frame = camera.read_frame()
+
+            detect_marker(frame, arucoDict, arucoParams)
+            
+            cv2.imshow("video", frame)
+
+            if (cv2.waitKey(1) == 27):
+    
+                break
+        except InterruptedError:
+            break
+
+    cv2.destroyAllWindows()
+    
